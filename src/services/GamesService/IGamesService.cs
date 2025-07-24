5d60efdf88e2a6134a1022fac93c5a2da182a3ba
sgnome <lilchim@protonmail.com>
2025-07-23 23:35:29 -0400

    Add IGamesService

---
+++ src/services/GamesService/IGamesService.cs
@@ -0,0 +1,28 @@
+using Sgnome.Models.Graph;
+using Sgnome.Models.Nodes;
+
+namespace GamesService;
+
+public interface IGamesService
+{
+    /// <summary>
+    /// Consumes a GameNode (self-domain) - resolves the node and returns enrichment pins
+    /// </summary>
+    /// <param name="partial">Partial game data to resolve</param>
+    /// <returns>Tuple of enrichment pins and resolved GameNode</returns>
+    Task<(IEnumerable<Pin> Pins, GameNode ResolvedNode)> Consume(GameNode partial);
+
+    /// <summary>
+    /// Consumes a LibraryNode (foreign-domain) - returns pins linking to game data
+    /// </summary>
+    /// <param name="library">Library node to analyze for game connections</param>
+    /// <returns>Collection of pins representing game-related information</returns>
+    Task<IEnumerable<Pin>> Consume(LibraryNode library);
+
+    /// <summary>
+    /// Consumes a PlayerNode (foreign-domain) - returns pins linking to game data
+    /// </summary>
+    /// <param name="player">Player node to analyze for game connections</param>
+    /// <returns>Collection of pins representing game-related information</returns>
+    Task<IEnumerable<Pin>> Consume(PlayerNode player);
+} 
