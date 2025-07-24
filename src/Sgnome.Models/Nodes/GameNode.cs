cf467d25438ac8c13d0ce2d2407c1bdf88a26b81
sgnome <lilchim@protonmail.com>
2025-07-23 23:35:01 -0400

    Update GameNode
    Remove old properties

--- src/Sgnome.Models/Nodes/GameNode.cs
+++ src/Sgnome.Models/Nodes/GameNode.cs
@@ -1,14 +1,21 @@
+using Sgnome.Models.Graph;
+
 namespace Sgnome.Models.Nodes;
 
 // Minimal game identity - relational data goes in pins
-public class GameNode
+public class GameNode : Node
 {
-    public int? SteamAppId { get; set; }
-    public string? EpicId { get; set; }
+    public string? InternalId { get; set; }
     public string? Name { get; set; }
     public string? IconUrl { get; set; }
     public string? LogoUrl { get; set; }
     
     // Core identity properties only
     public Dictionary<string, object> Identifiers { get; set; } = new();
+
+    public GameNode()
+    {
+        Type = "gameNode";
+        Data.NodeType = "game";
+    }
 } 
