1f15484c5da4214a962794d69b5f5a165d59b757
sgnome <lilchim@protonmail.com>
2025-07-31 21:37:21 -0400

    Add LibraryPresenter.ts

---
+++ src/Sgnome.Frontend/src/lib/presenters/LibraryPresenter.ts
@@ -0,0 +1,12 @@
+import { PIN_CONSTANTS } from "$lib/constants/pinTypes";
+import type { Pin } from "../types/graph";
+
+export class LibraryPresenter {
+
+    getLibraryLabel({pins}: {pins: Pin[]}): string {
+        let libraryPins = pins.filter(pin => pin.type === PIN_CONSTANTS.PIN_TYPES.LIBRARY_PINS.LIBRARY);
+        return libraryPins.length > 0 ? libraryPins[0].label : 'Library';
+    }
+
+
+}
