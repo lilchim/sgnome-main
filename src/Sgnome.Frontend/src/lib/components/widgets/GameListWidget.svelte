664aaf3fb8af48a280244c9cd44f71a840516432
sgnome <lilchim@protonmail.com>
2025-07-31 22:25:26 -0400

    Add GameListWidget.svelte

---
+++ src/Sgnome.Frontend/src/lib/components/widgets/GameListWidget.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Pin } from "$lib/types/graph";
+
+    export let input: { gamePins: Pin[] };
+</script>
+
+<div>
+    {#each input.gamePins as gamePin}
+        <div>
+            <h1>{gamePin.label}</h1>
+        </div>
+    {/each}
+</div>
