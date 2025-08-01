098cdc8a1c3951c7cefa67025c0ce03043d2e326
sgnome <lilchim@protonmail.com>
2025-07-31 23:21:53 -0400

    Update GameListWidget
    Add a search box for filtering and ability to select games to pin them

--- src/Sgnome.Frontend/src/lib/components/widgets/GameListWidget.svelte
+++ src/Sgnome.Frontend/src/lib/components/widgets/GameListWidget.svelte
@@ -1,23 +1,85 @@
 <script lang="ts">
     import type { Pin } from "$lib/types/graph";
     import { Handle, Position } from "@xyflow/svelte";
+    import { Separator } from "$lib/components/ui/separator";
+    import { Button } from "$lib/components/ui/button";
+    import { ListPlusIcon, ListXIcon } from "@lucide/svelte";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
 
     const { gamePins } = $props<{ gamePins: Pin[] }>();
 
-    const selectedGames = $state<Pin[]>([]);
+    let selectedGames = $state<Pin[]>([]);
+    let searchText = $state<string>("");
+
+    const unselectedGames = $derived(
+        gamePins.filter((gamePin: Pin) => !selectedGames.includes(gamePin)),
+    );
+
+    const filteredUnselectedGames = $derived(() => {
+        if (!searchText.trim()) {
+            return unselectedGames;
+        }
+
+        const searchLower = searchText.toLowerCase();
+        return unselectedGames.filter((gamePin: Pin) =>
+            gamePin.label.toLowerCase().includes(searchLower),
+        );
+    });
 </script>
 
-<div>
-    {#each gamePins as gamePin}
-        <div class="flex justify-between">
-            
-            <h1>{gamePin.label}</h1>
-            <Handle
-                id={gamePin.id}
-                type="source"
-                position={Position.Right}
-                class="pin-handle"
+<div class="flex flex-col gap-2">
+    <div>
+        <h1>Selected Games</h1>
+        {#each selectedGames as gamePin}
+            <div>
+                <div class="flex justify-between">
+                    <Button
+                        size="sm"
+                        variant="ghost"
+                        onclick={() => {
+                            selectedGames = selectedGames.filter(
+                                (g) => g.id !== gamePin.id,
+                            );
+                        }}
+                    >
+                        <ListXIcon />
+                    </Button>
+                    <h1>{gamePin.label}</h1>
+                    <Handle
+                        id={gamePin.id}
+                        type="source"
+                        position={Position.Right}
+                        class="pin-handle"
+                    />
+                </div>
+            </div>
+        {/each}
+    </div>
+    <Separator />
+    <div>
+        <h1>Available Games</h1>
+        <div class="flex w-full max-w-sm flex-col gap-1.5">
+            <Input
+                type="text"
+                id="search"
+                placeholder="Filter by Name"
+                bind:value={searchText}
             />
         </div>
-    {/each}
+        {#each filteredUnselectedGames() as gamePin}
+            <div class="flex items-center p-2">
+                <Button
+                    size="icon"
+                    variant="ghost"
+                    onclick={() => {
+                        selectedGames.push(gamePin);
+                    }}
+                >
+                    <ListPlusIcon />
+                </Button>
+                <span class="text-md">{gamePin.label}</span>
+            </div>
+        {/each}
+    </div>
 </div>
