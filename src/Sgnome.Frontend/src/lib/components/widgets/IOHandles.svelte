e3463e0dc6811b07833888277abf40159965e0ce
sgnome <lilchim@protonmail.com>
2025-07-31 21:33:35 -0400

    Add IOHandles.svelte

---
+++ src/Sgnome.Frontend/src/lib/components/widgets/IOHandles.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Handle, Position } from "@xyflow/svelte";
+
+    export let hostId: string;
+</script>
+
+<div class="flex justify-between">
+    <Handle
+        type="target"
+        position={Position.Left}
+        id={`${hostId}-input`}
+        class="pin-handle"
+    />
+    <Handle
+        type="source"
+        position={Position.Right}
+        id={`${hostId}-output`}
+        class="pin-handle"
+    />
+</div>
